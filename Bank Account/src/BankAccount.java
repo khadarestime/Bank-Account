fdec1f19bcd4021175057a3e040ca71579bd06a8
Khadar Estime <s1292235@monmouth.edu>
2020-11-04 18:24:36 -0500

    first commit

---
+++ Bank Account/src/BankAccount.java
@@ -0,0 +1,60 @@
+/**
+A bank account has a balance and a mechanism for
+applying interest or fees at the end of the month.
+*/
+public class BankAccount
+{
+private double balance;
+private double interestPct;
+
+/**
+   Constructs a bank account with zero balance.
+*/
+public BankAccount(double amount, double intpct) 
+{
+   balance = amount;
+   interestPct = intpct;
+}
+
+/**
+   Makes a deposit into this account.
+   @param amount the amount of the deposit
+*/
+public void deposit(double amount)
+{
+   balance = balance + amount;
+}
+
+/**
+ * Makes an instance variable to help calculate the interest
+ */
+public void calcInterest() 
+{
+	   
+}
+/**
+   Makes a withdrawal from this account, or charges a penalty if
+   sufficient funds are not available.
+   @param amount the amount of the withdrawal
+*/
+
+public void withdraw(double amount)
+{
+   if ((balance - amount) > 0) {
+ 	  balance = balance - amount;
+   } 
+   else {
+ 	  System.out.println("Insuffiecient Funds to Support Withdrawl");
+   }
+  
+}
+/**
+   Gets the current balance of this bank account.
+   @return the current balance
+*/
+public double getBalance()
+{
+   return balance;
+}
+
+}
---
+++ Bank Account/src/BankAccountTest.java
@@ -0,0 +1,31 @@
+
+import java.util.Scanner;
+public class BankAccountTest {
+
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		
+		System.out.println("Enter amount to start your account: ");
+		double newAmt = input.nextDouble();
+		
+		System.out.println("Enter Percentage of Interest: ");
+		double interest = input.nextDouble();
+		
+		BankAccount myBankAccount = new BankAccount(newAmt, interest);
+		
+		System.out.println("Enter Deposit Amount: ");
+		double Deposit = input.nextDouble();
+		myBankAccount.deposit(Deposit);
+		
+		System.out.println("Enter Withdraw Amount: ");
+		double Withdraw = input.nextDouble();
+		myBankAccount.withdraw(Withdraw);
+		
+		myBankAccount.calcInterest();
+		
+		System.out.println("Your Bank Account Balance is: " +myBankAccount.getBalance());
+		
+		
+		
+			}
+}
